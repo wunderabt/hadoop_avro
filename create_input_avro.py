161838a866363a98b39c7dc843461866c4b51db2
wunderabt <wunderabt@yahoo.co.in>
2018-12-21 17:00:41 +0100

    create compressed avros

---
+++ create_input_avro.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import avro.schema
+from avro.datafile import DataFileReader, DataFileWriter
+from avro.io import DatumReader, DatumWriter
+import glob
+import os.path
+
+schema = avro.schema.Parse(open("mp3s.avsc").read())
+
+
+for raw_file in glob.glob("./raw_data/*.mp3"):
+    avro_name = os.path.splitext(os.path.basename(raw_file))[0]
+    avro_file = os.path.join("input", avro_name + ".avro")
+    print("Processing {} --> {}".format(raw_file, avro_file))
+    with open(raw_file, "rb") as raw_fh, open(avro_file, "wb") as avro_fh:
+        writer = DataFileWriter(avro_fh, DatumWriter(), schema, codec="deflate")
+        writer.append({"name": avro_name, "file": raw_fh.read()})
+        writer.close()
+
+#reader = DataFileReader(open("/tmp/users.avro", "r"), DatumReader())
+#for user in reader:
+#    print user
+#reader.close()
